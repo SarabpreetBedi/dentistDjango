b1b907c4fc1523a0afdd545e07df76559dc7e4b8
Sarabpreet Bedi <bedisarabpreet@gmail.com>
2021-10-08 21:27:13 +0530

    tweaked app for heroku

--- dentist/settings.py
+++ dentist/settings.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'dentist.urls'
@@ -114,6 +118,8 @@ STATICFILES_DIRS =[
        
    ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # myaccount.google.com/lesssecureapps
 # accounts.google.com/DisplayUnlockCaptcha
 # myaccount.google.com/apppasswords
@@ -124,6 +130,8 @@ EMAIL_HOST_USER = 'bedisarabpreet@gmail.com'
 EMAIL_HOST_PASSWORD = 'sarabpreet@1982'
 EMAIL_USE_TLS = True
 #EMAIL_USE_SSL = False
+
+django_heroku.settings(locals())
 
 
 # Default primary key field type
